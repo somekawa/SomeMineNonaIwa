145f90a9e5e297b1105c9bb4f553481f1e2dae7e
somekawa <some>
2021-04-26 15:21:12 +0900

    いれてみた

--- Player/playerController.cs
+++ Player/playerController.cs
@@ -9,6 +9,7 @@ public class playerController : MonoBehaviour
     [SerializeField]
     private float speed = 3.0f;
     private float gravity = 9.8f;
+    private bool walkFlg = false;
 
     void Start()
     {
@@ -25,9 +26,24 @@ public class playerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+
+        if(direction.x != 0 || direction.z != 0)
+        {
+            walkFlg = true;
+        }
+        else
+        {
+            walkFlg = false;
+        }
+
         Vector3 velocity = direction * speed;
         velocity.y -= gravity;
         velocity = transform.transform.TransformDirection(velocity);
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    public bool GetWalkFlg()
+    {
+        return walkFlg;
     }
 }
---
+++ effectScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effectScript : MonoBehaviour
+{
+    [SerializeField]
+    private int count = 0;
+    private bool countFlg = false;
+    private bool onceFlg  = false;
+    private float playTime = 0.0f;
+    private GameObject effect = null;
+
+    private GameObject player;
+    private playerController plSc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        if (player != null)
+        {
+            plSc = player.GetComponent<playerController>();
+        }
+        else
+        {
+            Debug.Log("Playerがnullでエラー");
+        }
+
+        if (plSc == null)
+        {
+            Debug.Log("plScがnullでエラー");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 120秒でフラグを立てる
+        count++;
+        if (count >= 120 && !countFlg)
+        {
+            countFlg = true;
+            onceFlg = true;
+        }
+
+        // フラグがtrueの時、エフェクトを再生させる
+        if (countFlg)
+        {
+            if (onceFlg)
+            {
+                if (effect == null)
+                {
+                    effect = GameObject.Find("FootEffect");
+                    if (effect == null)
+                    {
+                        Debug.Log("FootEffectがnullでエラー");
+                    }
+                }
+
+                // プレイヤーが移動しているときだけ再生するように設定
+                if (plSc.GetWalkFlg())
+                {
+                    effect.GetComponent<ParticleSystem>().Play();
+                }
+
+                count = 0;
+                countFlg = false;
+            }
+        }
+
+        if (onceFlg)
+        {
+            playTime += Time.deltaTime;
+        }
+
+        if (playTime >= 0.5f)
+        {
+            if (effect != null)
+            {
+                effect.GetComponent<ParticleSystem>().Stop();
+                playTime = 0.0f;
+            }
+            effect = null;
+            onceFlg = false;
+        }
+    }
+}
