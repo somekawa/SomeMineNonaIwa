13316a00c5e40fa927c71b22a4a90ab9d00e15b7
saki <aaa>
2021-05-13 14:31:17 +0900

    ライト範囲内に敵がいるかのチェック用

---
+++ Scripts/Battery/tLightRange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tLightRange : MonoBehaviour
+{
+    private bool hitCheck_ = false;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            hitCheck_ = true;
+            Debug.Log("ライトの範囲内に敵を確認しました。");
+       }
+    }
+}
