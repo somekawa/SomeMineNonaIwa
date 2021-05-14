9b41e41cdef20290d7f50a66df7cdaf984817fc3
saki <aaa>
2021-05-14 13:44:19 +0900

    正気度取得変数、表示変数追加

--- Scripts/Scene/ClearScene.cs
+++ Scripts/Scene/ClearScene.cs
@@ -2,19 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ClearScene : MonoBehaviour
 {
+
+    private int remainingSanit_;   // 取得した正気度を保存
+
+
+    public Text sanitText;    // 正気度表示用
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        remainingSanit_ = (int)SanitMng.sanit_;
+        sanitText.text =  "のこりSAN ：" +remainingSanit_;        // 正気度を表示
+        Debug.Log("残り正気度" + remainingSanit_);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnClickButton()
