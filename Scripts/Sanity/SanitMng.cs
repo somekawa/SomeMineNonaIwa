57d9ea194e1fbb830aecc033d9b352875348da7a
mia <kajiki>
2021-05-14 18:42:48 +0900

    ・正気度修正 Hide Control_には『Player』を設定してください。 D_recoveryTime_は耐久時間1秒回復にかかる時間なので最低でも『1』を設定してください。

--- Scripts/Sanity/SanitMng.cs
+++ Scripts/Sanity/SanitMng.cs
@@ -8,6 +8,7 @@ public class SanitMng : MonoBehaviour
 {
     public GameObject spotLight_;
     public NoiseControl noiseControl_;
+    public HideControl hideControl_;
     public GameObject text_;
 
     private float maxSanit_       = 100.0f;         // 最大正気度
@@ -15,19 +16,19 @@ public class SanitMng : MonoBehaviour
     public static float sanit_    = 0.0f;         // 正気度 
 
     private bool oldLightFlag_;
-    private float onTime_;                          // 懐中電灯をオンにした時間
-    private float offTime_;                         // 懐中電灯をオフにした時間
+    private float onTime_         = 0.0f;           // 懐中電灯をオンにした時間
+    private float offTime_        = 0.0f;           // 懐中電灯をオフにした時間
     public float d_timeMax_;                        // 懐中電灯をオフにしてから耐久出来る最大時間
-    public float d_timeMin_;                        // 懐中電灯をオフにしてから耐久出来る最小時間
-    private float d_time_;                           // 懐中電灯をオフにしてから耐久出来る実際の時間
-    private float d_recoveryTime_ = 10.0f;          // 耐久時間回復にかかる時間
-    private float d_nowTime_;                       // 耐久出来る残り時間(確認用のため削除する予定)
+    private float d_time_;                          // 懐中電灯をオフにしてから耐久出来る実際の時間
+    public float d_recoveryTime_;                   // 耐久時間1秒回復にかかる時間
+    private float d_nowTime_;                       // 耐久出来る残り時間
+
+    private bool recoveryFlag_ = false;             // 回復中
 
     // Start is called before the first frame update
     void Start()
     {
-        //text_ = gameObject.transform.Find("SanitCanvas/text").gameObject;
-
+        oldLightFlag_ = !spotLight_.activeSelf;
         sanit_ = maxSanit_;
         d_time_ = d_timeMax_;
         d_nowTime_ = d_time_;
@@ -60,57 +61,87 @@ public class SanitMng : MonoBehaviour
             return;
         }
 
+        if(hideControl_.GetHideFlg())
+        {
+            // 箱の中に隠れている
+            DurableRecovery();
+            DurableReset();
+
+            return;
+        }
+
         bool lightFlag = spotLight_.activeSelf;
         if (oldLightFlag_ != lightFlag)
         {
-            if (!lightFlag)
+            if (lightFlag)
             {
-                onTime_ = 0.0f;
-                offTime_ = Time.time;
+                DurableReset();
             }
             else
             {
-                if ((offTime_ != 0.0f) && (Time.time - offTime_ > d_time_))
-                {
-                    // 正気度低下した場合は耐久時間を少なくする
-                    d_time_ = d_timeMin_;
-                }
-                onTime_ = Time.time;
-                offTime_ = 0.0f;
+                d_time_ = d_nowTime_;
+                onTime_ = 0.0f;
+                offTime_ = Time.time;
+                recoveryFlag_ = false;
             }
         }
         else
         {
-            if (!oldLightFlag_) 
+            if (oldLightFlag_) 
             {
-
+                DurableRecovery();
+            }
+            else
+            {
                 if (offTime_ != 0.0f)
                 {
                     d_nowTime_ = d_time_ - (Time.time - offTime_);
                 }
 
-                if ((offTime_ != 0.0f) && (Time.time - offTime_ > d_time_))
+                if ((offTime_ != 0.0f) && (d_nowTime_ <= 0.0f)) 
                 {
                     sanit_ -= 0.1f;
                     d_nowTime_ = 0.0f;
                 }
             }
-            else
-            {
-                // 耐久出来る時間を回復
-                if (d_time_ < d_timeMax_) 
-                {
-                    d_time_ = (((d_timeMax_ - d_timeMin_) / d_recoveryTime_) * (Time.time - onTime_)) + d_timeMin_;
-                }
-                else
-                {
-                    d_time_ = d_timeMax_;
-                }
-
-                d_nowTime_ = d_time_;
-            }
         }
 
         oldLightFlag_ = lightFlag;
+    }
+
+    // 耐久時間のリセット
+    private void DurableReset()
+    {
+        if (recoveryFlag_) 
+        {
+            return;
+        }
+
+        d_time_ = d_nowTime_;
+        if (d_time_ < d_timeMax_)
+        {
+            recoveryFlag_ = true;
+        }
+        onTime_ = Time.time;
+        offTime_ = 0.0f;
+    }
+
+    // 耐久時間の回復
+    private void DurableRecovery()
+    {
+        if(!recoveryFlag_)
+        {
+            return;
+        }
+
+        // 耐久出来る時間を回復
+        if (d_nowTime_ < d_timeMax_)
+        {
+            d_nowTime_ = ((Time.time - onTime_) / d_recoveryTime_) + d_time_;
+        }
+        else
+        {
+            d_nowTime_ = d_timeMax_;         
+        }
     }
 }
