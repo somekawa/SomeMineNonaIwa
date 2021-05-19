025394b0624be1cfde56378085325808b61f5cc5
mia <kajiki>
2021-05-19 14:19:17 +0900

    入れ忘れ

---
+++ Scripts/Scene/GameOverScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverScene : MonoBehaviour
+{
+    private NoiseControl noise_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        noise_ = gameObject.GetComponent<NoiseControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.value * 100.0f < 0.1f) 
+        {
+            noise_.DiscoveryNoise();
+        }
+    }
+}
