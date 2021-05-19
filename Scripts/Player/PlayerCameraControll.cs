78495f529e24843d3d4e21efa34ecda2fc7d7e5f
DAISUKEIWASAKI0615 <1816004@s.asojuku.ac.jp>
2021-05-19 15:15:14 +0900

    プレイヤーカメラ用のスクリプト追加

---
+++ Scripts/Player/PlayerCameraControll.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraControll : MonoBehaviour
+{
+    public float x_sensi;                      // マウスのX座標の感度格納用
+    public float y_sensi;                      // マウスのY座標の感度格納用
+    public GameObject playerCamera;       　   // 動かすカメラ格納用
+
+    private float x_Rotation_;
+    private float y_Rotation_;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameracon();
+    }
+
+
+    private void cameracon()
+    {
+        x_Rotation_ = Input.GetAxis("Mouse X");                   // マウスのX座標の取得
+        y_Rotation_ = Input.GetAxis("Mouse Y");                   // マウスのY座標の取得
+
+        x_Rotation_ = x_Rotation_ * x_sensi;                      // カメラのX座標の回転速度の計算
+        y_Rotation_ = y_Rotation_ * y_sensi;                      // カメラのY座標の回転速度の計算
+
+        this.transform.Rotate(0, x_Rotation_, 0);                 // X座標の回転はスクリプトをアタッチしているオブジェクトに
+        playerCamera.transform.Rotate(-y_Rotation_, 0, 0);      　// Y座標の回転は子オブジェクトのカメラに
+    }
+}
