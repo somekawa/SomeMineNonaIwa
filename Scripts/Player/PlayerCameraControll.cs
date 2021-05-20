4131c69f12d9e226a379d4bcf3d5891d997b4613
DAISUKEIWASAKI0615 <1816004@s.asojuku.ac.jp>
2021-05-20 10:37:13 +0900

    全体マップを表示中にカメラが動かないよう調整

--- Scripts/Player/PlayerCameraControll.cs
+++ Scripts/Player/PlayerCameraControll.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class PlayerCameraControll : MonoBehaviour
 {
-    public float x_sensi;                      // マウスのX座標の感度格納用
-    public float y_sensi;                      // マウスのY座標の感度格納用
-    public GameObject playerCamera;       　   // 動かすカメラ格納用
+    public float x_sensi;                          // マウスのX座標の感度格納用
+    public float y_sensi;                          // マウスのY座標の感度格納用
+    public GameObject playerCamera;                // 動かすカメラ格納用
+    public CameraController cameraController_;     
 
     private float x_Rotation_;
     private float y_Rotation_;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,11 +26,14 @@ public class PlayerCameraControll : MonoBehaviour
 
     private void cameracon()
     {
-        x_Rotation_ = Input.GetAxis("Mouse X");                   // マウスのX座標の取得
-        y_Rotation_ = Input.GetAxis("Mouse Y");                   // マウスのY座標の取得
+        if(cameraController_.FullMapFlag()==false)
+        {
+            x_Rotation_ = Input.GetAxis("Mouse X");                   // マウスのX座標の取得
+            y_Rotation_ = Input.GetAxis("Mouse Y");                   // マウスのY座標の取得
 
-        x_Rotation_ = x_Rotation_ * x_sensi;                      // カメラのX座標の回転速度の計算
-        y_Rotation_ = y_Rotation_ * y_sensi;                      // カメラのY座標の回転速度の計算
+            x_Rotation_ = x_Rotation_ * x_sensi;                      // カメラのX座標の回転速度の計算
+            y_Rotation_ = y_Rotation_ * y_sensi;                      // カメラのY座標の回転速度の計算
+        }
 
         this.transform.Rotate(0, x_Rotation_, 0);                 // X座標の回転はスクリプトをアタッチしているオブジェクトに
         playerCamera.transform.Rotate(-y_Rotation_, 0, 0);      　// Y座標の回転は子オブジェクトのカメラに
