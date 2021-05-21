d10fb4dc71d5ebc08fc2bedc4514a35f93164699
mia <kajiki>
2021-05-21 10:10:10 +0900

    隠れてるときは箱のOutlineをオフにするように修正

--- Scripts/HideBox/HideBox.cs
+++ Scripts/HideBox/HideBox.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class HideBox : MonoBehaviour
 {
     private Outline outline_;
+    private HideControl hideControl_;
+
     // Start is called before the first frame update
     void Start()
     {
         outline_ = gameObject.GetComponent<Outline>();
         outline_.enabled = false;
+
+        GameObject obj = GameObject.Find("Player").gameObject;
+        hideControl_ = obj.GetComponent<HideControl>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(hideControl_.GetHideFlg())
+        {
+            outline_.enabled = false;
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player") 
         {
