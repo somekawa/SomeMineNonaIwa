cb93fbd4d36fb68a080f490b86eba731ed3512a6
DAISUKEIWASAKI0615 <1816004@s.asojuku.ac.jp>
2021-05-28 13:10:37 +0900

    ItemRotateの追加、その影響でPig更新

---
+++ Scripts/Item/ItemRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemRotate : MonoBehaviour
+{
+    private GameObject targetObject_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetObject_ = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(targetObject_.transform);
+    }
+}
