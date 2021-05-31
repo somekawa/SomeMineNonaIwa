7f4386ba77b6bfd0cb667649c856d7398d6dd177
somekawa <1816044@s.asojuku.ac.jp>
2021-05-31 10:43:36 +0900

    Prefab更新,アウトライン更新

---
+++ Scripts/Outline/OutlineMng.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineMng : MonoBehaviour
+{
+    private Outline outline_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        outline_ = gameObject.GetComponent<Outline>();
+        outline_.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "ItemHitArea")
+        {
+            outline_.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "ItemHitArea")
+        {
+            outline_.enabled = false;
+        }
+    }
+
+}
