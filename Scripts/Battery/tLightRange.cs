9211fb527fa126d7696562fce8bb036838f7d9fe
mia <kajiki>
2021-06-02 15:12:38 +0900

    敵にライトを当ててから正気度低下までの猶予追加

--- Scripts/Battery/tLightRange.cs
+++ Scripts/Battery/tLightRange.cs
@@ -8,7 +8,11 @@ public class tLightRange : MonoBehaviour
 
     private GameObject slenderMan_;
     private SlenderManCtl slenderManCtl_;
-    private bool hitCheck_  = false;
+    private bool hitFlag_   = false;
+
+    private bool rangeFlag_     = false;    // 範囲内か
+    private float rangeTime_    = 0.0f;     // 範囲内に入ってからの時間
+    private float rangeMaxTime_ = 0.5f;     // 範囲内に入ってからの猶予時間
 
     void Start()
     {
@@ -22,20 +26,42 @@ public class tLightRange : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Enemy")
+        if (other.gameObject.tag != "Enemy")
+        {
+            return;
+        }
+        if (!rangeFlag_)
+        {
+            rangeFlag_ = true;
+            rangeTime_ = 0.0f;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag != "Enemy")
+        {
+            return;
+        }
+
+        rangeTime_ += Time.deltaTime;
+        Debug.Log("範囲内時間：" + rangeTime_);
+
+        // 画面揺れ処理追加予定
+
+        if (rangeTime_ >= rangeMaxTime_)
         {
             slenderManCtl_.inSightFlag = true;
-            //hitCheck_ = true;
 
-            if(barrier.GetBarrierItemFlg())
+            if (barrier.GetBarrierItemFlg())
             {
-                hitCheck_ = false;
+                hitFlag_ = false;
                 barrier.SetBarrierItemFlg(false);
                 Debug.Log("防御アイテムを使用しました。");
             }
             else
             {
-                hitCheck_ = true;
+                hitFlag_ = true;
             }
             Debug.Log("ライトの範囲内に敵を確認しました。");
         }
@@ -45,13 +71,14 @@ public class tLightRange : MonoBehaviour
     {
         if (other.gameObject.tag == "Enemy")
         {
-            hitCheck_ = false;
+            hitFlag_ = false;
+            rangeFlag_ = false;
             Debug.Log("敵がライトの範囲外にいきました。");
         }
     }
 
     public bool GetHitCheck()
     {
-        return hitCheck_;
+        return hitFlag_;
     }
 }
