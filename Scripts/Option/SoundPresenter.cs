9c9391afa8fda2260ed890fe439af196c0a6bcd7
DAISUKEIWASAKI0615 <1816004@s.asojuku.ac.jp>
2021-06-02 15:26:44 +0900

    Titleで使ってるScriptの追加

---
+++ Scripts/Option/SoundPresenter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SoundPresenter : MonoBehaviour
+{
+    //BGM
+    public TextMeshProUGUI bgmVolumeText;       //BGMMenuViewのvolumeTextを取得
+    public Slider bgmSlider;                    //BGMMenuViewのsliderを取得
+
+    //SE
+    public TextMeshProUGUI seVolumeText;        //SEMenuViewのvolumeTextを取得
+    public Slider seSlider;                     //SEMenuViewのsliderを取得
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        //BGMを再生
+        OptionScript.GetInstance().PlayBGM(0);
+    }
+
+    //BGMMenuViewのSliderを動かしたときに呼び出す関数を作成
+    public void OnChangedBGMSlider()
+    {
+        //Sliderの値に応じてBGMを変更
+        OptionScript.GetInstance().BGMVolume = bgmSlider.value;
+        //volumeTextの値をSliderのvalueに変更
+        bgmVolumeText.text = string.Format("{0:0}", bgmSlider.value * 100);
+    }
+
+    //SEMenuViewのSliderを動かしたときに呼び出す関数を作成
+    public void OnChangedSESlider()
+    {
+        //Sliderの値に応じてSEを変更
+        OptionScript.GetInstance().SEVolume = seSlider.value;
+        //volumeTextの値をSliderのvalueに変更
+        seVolumeText.text = string.Format("{0:0}", seSlider.value * 100);
+    }
+
+    //Buttonを押したときに呼ばれる関数
+    public void OnPushButton()
+    {
+        //SEを再生
+        OptionScript.GetInstance().PlaySound(0);
+    }
+}
--- Scripts/Scene/TitleScene.cs
+++ Scripts/Scene/TitleScene.cs
@@ -6,19 +6,27 @@ using UnityEngine.UI;
 
 public class TitleScene : MonoBehaviour
 {
+    public Canvas titleCanvas;
+    public Canvas optionCanvas;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        titleCanvas.gameObject.SetActive(true);
+        optionCanvas.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if (Input.GetKeyDown(KeyCode.Tab))            // キーが押されたとき
+        //{
+        //    titleCanvas.gameObject.SetActive(false);
+        //    optionCanvas.gameObject.SetActive(true);
+        //}
     }
 
-    public void TransitionGame()
+        public void TransitionGame()
     {
         SceneManager.LoadScene("MainScene");
     }
@@ -26,5 +34,16 @@ public class TitleScene : MonoBehaviour
     public void TransitionTutorial()
     {
         SceneManager.LoadScene("TutorialScene");
+    }
+
+    public void ReturnOption()
+    {
+        titleCanvas.gameObject.SetActive(true);
+        optionCanvas.gameObject.SetActive(false);
+    }
+    public void HeadOption()
+    {
+        titleCanvas.gameObject.SetActive(false);
+        optionCanvas.gameObject.SetActive(true);
     }
 }
