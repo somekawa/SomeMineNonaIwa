1b9e0699e7275cd47f986f4f38050a4fb7012ce0
mia <kajiki>
2021-06-04 14:59:51 +0900

    正気度減少のタイミングに猶予追加、ついでにカメラの揺れを今できてるところまで追加 CameraShake.csはMainCameraに追加してtLightRangeで設定するとスレンダーマンが懐中電灯の範囲内にいる間はカメラが揺れるようになります。

--- Scripts/Battery/tLightRange.cs
+++ Scripts/Battery/tLightRange.cs
@@ -5,9 +5,16 @@ using UnityEngine.AI;
 
 public class tLightRange : MonoBehaviour
 {
+    public Barrier barrier;
+    public CameraShake cameraShake_;
+
     private GameObject slenderMan_;
     private SlenderManCtl slenderManCtl_;
-    private bool hitCheck_  = false;
+    private bool hitFlag_       = false;
+
+    private bool rangeFlag_     = false;    // 範囲内か
+    private float rangeTime_    = 0.0f;     // 範囲内に入ってからの時間
+    private float rangeMaxTime_ = 0.5f;     // 範囲内に入ってからの猶予時間
 
     void Start()
     {
@@ -21,27 +28,68 @@ public class tLightRange : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Enemy")
+        if (other.gameObject.tag != "Enemy")
         {
-            slenderManCtl_.navMeshAgent_.ResetPath();
-            slenderManCtl_.status = SlenderManCtl.Status.NULL;
-            //slenderManCtl_.inSightFlag = true;
-            hitCheck_ = true;
+            return;
+        }
+        if (!rangeFlag_)
+        {
+            rangeFlag_ = true;
+            rangeTime_ = 0.0f;
+        }
+        slenderManCtl_.navMeshAgent_.ResetPath();
+        slenderManCtl_.status = SlenderManCtl.Status.NULL;
+        //slenderManCtl_.inSightFlag = true;       
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag != "Enemy")
+        {
+            return;
+        }
+
+        rangeTime_ += Time.deltaTime;
+        Debug.Log("範囲内時間：" + rangeTime_);
+
+        // 画面揺れ処理追加予定
+        cameraShake_.Shake(other.gameObject);
+
+        if (rangeTime_ >= rangeMaxTime_)
+        {
+            //@slenderMan このタイミングでワープお願いします。
+
+            if (barrier.GetBarrierItemFlg())
+            {
+                hitFlag_ = false;
+                barrier.SetBarrierItemFlg(false);
+                Debug.Log("防御アイテムを使用しました。");
+            }
+            else
+            {
+                hitFlag_ = true;
+            }
             Debug.Log("ライトの範囲内に敵を確認しました。");
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Enemy")
+        if (other.gameObject.tag != "Enemy")
         {
-            hitCheck_ = false;
-            Debug.Log("敵がライトの範囲外にいきました。");
+            return;
         }
+
+        //@slenderMan このタイミングで動きお願いします。
+
+        hitFlag_ = false;
+        rangeFlag_ = false;
+        cameraShake_.OffShake();
+        Debug.Log("敵がライトの範囲外にいきました。");
     }
 
     public bool GetHitCheck()
     {
-        return hitCheck_;
+        return hitFlag_;
     }
 }
