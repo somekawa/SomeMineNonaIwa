319921bf75e83a394ddb961d5cf583732765de9f
saki <aaa>
2021-06-17 10:57:18 +0900

    ポーズ中でも時間経過するようにしました。 時間を計るコードをPauseからGameSceneに移しました。

--- Scripts/Scene/GameScene.cs
+++ Scripts/Scene/GameScene.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameScene : MonoBehaviour
 {
     private bool pauseFlag;
-    // Start is called before the first frame update
+
+    private int minute;           // 何分か
+    public Text timerText;        // タイマー表示用テキスト
+    private int seconds_;         // 何秒か
+    private bool secondsAddFlag_; // 秒数が足されたか
+    private bool resetFlag_;      // 経過時間をリセットして良いか
+
     void Start()
     {
-
+        seconds_ = 0;
+        secondsAddFlag_ = false;
+        resetFlag_ = false;
+        StartCoroutine("Coroutine");
     }
 
     // Update is called once per frame
@@ -25,10 +36,47 @@ public class GameScene : MonoBehaviour
                 pauseFlag = false;
             }
         }
+
+
+        // チュートリアルからメインに移った時だけはいる
+        if (resetFlag_==false&& SceneManager.GetActiveScene().name == "MainScene")
+        {
+            resetFlag_ = true;
+            seconds_ = 0;
+            StartCoroutine("Coroutine");
+        }
+
+        // 毎フレーム呼ばれないようにフラグで管理
+        if (secondsAddFlag_ == true)
+        {
+            // count_60以上＝1分
+            if (seconds_ >= 60f)
+            {
+                minute++;
+                seconds_ = seconds_ - 60;
+            }
+            StartCoroutine("Coroutine");
+            secondsAddFlag_ = false;
+        }
+        timerText.text = minute.ToString("00") + ":" + (seconds_).ToString("00");
+        Debug.Log("Coroutine"+ seconds_);
     }
 
     public bool GetPauseFlag()
     {
         return pauseFlag;
     }
+
+    private IEnumerator Coroutine()
+    {
+        //処理１
+        Debug.Log("Start");
+
+        //１秒待機
+        // WaitForSecondsRealtimeにすることでTimeScaleの影響を受けない
+        yield return new WaitForSecondsRealtime(1.0f);
+        seconds_++;        // 1秒経過したらcount_が+1される
+        secondsAddFlag_ = true;
+    }
+
 }
