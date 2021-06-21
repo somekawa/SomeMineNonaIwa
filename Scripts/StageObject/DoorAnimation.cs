ca699c7455ba462c634edec78847049e3b60f7fb
somekawa <1816044@s.asojuku.ac.jp>
2021-06-21 13:54:50 +0900

    扉の鎖と南京錠が外れたときに扉がアニメーションするように設定

--- Scripts/StageObject/DoorAnimation.cs
+++ Scripts/StageObject/DoorAnimation.cs
@@ -10,30 +10,36 @@ public class DoorAnimation : MonoBehaviour
 
     private float minAngle_;            // 回転する前の角度
     private float maxAngle_;            // 回転した後の角度
-    private float angle_;                // 回転角度格納用変数
+    private float angle_;               // 回転角度格納用変数
 
     // Start is called before the first frame update
     void Start()
     {
         minAngle_ = door.transform.rotation.y;
         maxAngle_ = 90;
-        openFlag = true;
+        openFlag = false;
         closeFlag = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // 鎖と南京錠の数を調べて、0以下の時に扉がアニメーションするようにフラグを立てる
+        if(transform.Find("Door").childCount <= 0)
+        {
+            openFlag = true;
+        }
+
         if (openFlag == true)
         {
             angle_ = Mathf.LerpAngle(minAngle_, maxAngle_, Time.time);
             door.transform.eulerAngles = new Vector3(0, angle_, 0);
-            if(door.transform.eulerAngles== new Vector3(0, maxAngle_, 0))
+            if(door.transform.eulerAngles == new Vector3(0, maxAngle_, 0))
             {
                 openFlag = false;
             }
         }
-        if(closeFlag==true)
+        else if(closeFlag == true)
         {
             angle_ = Mathf.LerpAngle(maxAngle_, minAngle_, Time.time);
             door.transform.eulerAngles = new Vector3(0, angle_, 0);
@@ -41,6 +47,10 @@ public class DoorAnimation : MonoBehaviour
             {
                 closeFlag = false;
             }
+        }
+        else
+        {
+            return;
         }
     }
 }
