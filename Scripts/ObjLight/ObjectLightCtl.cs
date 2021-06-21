a78fa3212f416f459d95b7913a1c77fef49fb18b
saki <aaa>
2021-06-21 15:20:18 +0900

    チュートリアル中は常時点灯状態にしました

--- Scripts/ObjLight/ObjectLightCtl.cs
+++ Scripts/ObjLight/ObjectLightCtl.cs
@@ -1,23 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ObjectLightCtl : MonoBehaviour
 {
-    public float displayRange;
-
-    //public GameObject comingObject;     // 自身との距離を計算するターゲットオブジェクト格納用
-    private GameObject comingObject;
+    public GameObject comingObject;     // 自身との距離を計算するターゲットオブジェクト格納用
 
     private GameObject cameraControll_;
     private CameraController cameraController_;
     private GameObject objectLight_;     // 発光するための子オブジェクト格納用
-    private bool lightFlag;
+
+    const float displayRange = 20;
 
     // Start is called before the first frame update
     void Start()
     {
-        comingObject = GameObject.Find("Player");
         cameraControll_ = GameObject.Find("CameraControll");
         cameraController_ = cameraControll_.GetComponent<CameraController>();
         objectLight_ = gameObject.GetComponentInChildren<Light>().gameObject;     // 子オブジェクトのlightを取得
@@ -26,26 +24,19 @@ public class ObjectLightCtl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // 距離を計算
-        if (Vector3.Distance(gameObject.transform.position, comingObject.transform.position) > displayRange ||
+        // チュートリアル中はずっと点灯させるため
+        if (SceneManager.GetActiveScene().name != "TutorialScene")
+        {
+            // 距離を計算
+            if (Vector3.Distance(gameObject.transform.position, comingObject.transform.position) > displayRange ||
             cameraController_.FullMapFlag() == true)
-        {
-            objectLight_.SetActive(false);
-            if (this.gameObject.tag == "Battery" ||
-                this.gameObject.tag == "BarrierItem" ||
-                this.gameObject.tag == "InductionItem" ||
-                this.gameObject.tag == "EscapeItem")
             {
-                if (lightFlag == true)
-                {
-                    objectLight_.SetActive(true);
-                }
+                objectLight_.SetActive(false);
             }
-        }
-        else
-        {
-            objectLight_.SetActive(true);
-            lightFlag = true;
+            else
+            {
+                objectLight_.SetActive(true);
+            }
         }
     }
 }
