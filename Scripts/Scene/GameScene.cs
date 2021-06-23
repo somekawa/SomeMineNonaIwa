7d1b02f94d3d03a6b652fd76f5a5b079b0acf75a
saki <aaa>
2021-06-23 15:38:59 +0900

    ゲームシーン中の時間計測がおかしかったので直しました

--- Scripts/Scene/GameScene.cs
+++ Scripts/Scene/GameScene.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class GameScene : MonoBehaviour
 {
@@ -11,17 +10,17 @@ public class GameScene : MonoBehaviour
 
     private bool pauseFlag;
 
-    private int minute;           // 何分か
+    // クリアシーンで使うため分と秒はpublicに
+    public static int minute;           // 何分か
+    public static int seconds;         // 何秒か
     public Text timerText;        // タイマー表示用テキスト
-    private int seconds_;         // 何秒か
     private bool secondsAddFlag_; // 秒数が足されたか
-    private bool resetFlag_;      // 経過時間をリセットして良いか
 
     void Start()
     {
-        seconds_ = 0;
+        minute = 0;
+        seconds = 0;
         secondsAddFlag_ = false;
-        resetFlag_ = false;
         StartCoroutine("Coroutine");
     }
 
@@ -41,28 +40,20 @@ public class GameScene : MonoBehaviour
         }
         batteryText.text = "電池残量" + batteryScript.ReturnBatteryRest()+"%";
 
-        // チュートリアルからメインに移った時だけはいる
-        if (resetFlag_==false&& SceneManager.GetActiveScene().name == "MainScene")
-        {
-            resetFlag_ = true;
-            seconds_ = 0;
-            StartCoroutine("Coroutine");
-        }
-
         // 毎フレーム呼ばれないようにフラグで管理
         if (secondsAddFlag_ == true)
         {
             // count_60以上＝1分
-            if (seconds_ >= 60f)
+            if (seconds >= 60f)
             {
                 minute++;
-                seconds_ = seconds_ - 60;
+                seconds = seconds - 60;
             }
             StartCoroutine("Coroutine");
             secondsAddFlag_ = false;
         }
-        timerText.text = minute.ToString("00") + ":" + (seconds_).ToString("00");
-        Debug.Log("Coroutine"+ seconds_);
+        timerText.text = minute.ToString("00") + ":" + (seconds).ToString("00");
+        Debug.Log("Coroutine"+ seconds);
     }
 
     public bool GetPauseFlag()
@@ -78,7 +69,7 @@ public class GameScene : MonoBehaviour
         //１秒待機
         // WaitForSecondsRealtimeにすることでTimeScaleの影響を受けない
         yield return new WaitForSecondsRealtime(1.0f);
-        seconds_++;        // 1秒経過したらcount_が+1される
+        seconds++;        // 1秒経過したらcount_が+1される
         secondsAddFlag_ = true;
     }
 
