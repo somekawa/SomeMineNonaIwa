3651fbef6355ffbe094977f8eb2f79d3f3d12b7b
DAISUKEIWASAKI0615 <1816004@s.asojuku.ac.jp>
2021-06-24 13:31:05 +0900

    GameSceneの変数名の更新

--- Scripts/Scene/GameScene.cs
+++ Scripts/Scene/GameScene.cs
@@ -2,66 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameScene : MonoBehaviour
 {
     public tBatteryScript batteryScript;
     public Text batteryText;
-    public Text keyText;
-    public PlayerCollision playerColScript;
+    public Text keyText;                         // 現在取得している鍵の数
+    public PlayerCollision playerColScript;      // itemhitareaのスクリプト
+    public Text timerText;                       // タイマー表示用テキスト
 
-    private bool pauseFlag;
-
-    // クリアシーンで使うため分と秒はpublicに
-    public static int minute;           // 何分か
-    public static int seconds;         // 何秒か
-    public Text timerText;        // タイマー表示用テキスト
-    private bool secondsAddFlag_; // 秒数が足されたか
+    private bool pauseFlag_;
+    private int minute_;                          // 何分か
+    private int seconds_;                        // 何秒か
+    private bool secondsAddFlag_;                // 秒数が足されたか
+    private bool resetFlag_;                     // 経過時間をリセットして良いか
 
     void Start()
     {
-        minute = 0;
-        seconds = 0;
+        seconds_ = 0;
         secondsAddFlag_ = false;
+        resetFlag_ = false;
         StartCoroutine("Coroutine");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //SceneManager.LoadScene("ClearScene");
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            if(pauseFlag != true)
+            if(pauseFlag_ != true)
             {
-                pauseFlag = true;
+                pauseFlag_ = true;
             }
             else
             {
-                pauseFlag = false;
+                pauseFlag_ = false;
             }
         }
         batteryText.text = "電池残量" + batteryScript.ReturnBatteryRest()+"%";
-        keyText.text = "(" + playerColScript.GetkeyItemCnt() + "/ 8)";
+
+        // チュートリアルからメインに移った時だけはいる
+        if (resetFlag_==false&& SceneManager.GetActiveScene().name == "MainScene")
+        {
+            resetFlag_ = true;
+            seconds_ = 0;
+            StartCoroutine("Coroutine");
+        }
 
         // 毎フレーム呼ばれないようにフラグで管理
         if (secondsAddFlag_ == true)
         {
             // count_60以上＝1分
-            if (seconds >= 60f)
+            if (seconds_ >= 60f)
             {
-                minute++;
-                seconds = seconds - 60;
+                minute_++;
+                seconds_ = seconds_ - 60;
             }
             StartCoroutine("Coroutine");
             secondsAddFlag_ = false;
         }
-        timerText.text = minute.ToString("00") + ":" + (seconds).ToString("00");
-        Debug.Log("Coroutine"+ seconds);
+        timerText.text = minute_.ToString("00") + ":" + (seconds_).ToString("00");
+        Debug.Log("Coroutine"+ seconds_);
     }
 
     public bool GetPauseFlag()
     {
-        return pauseFlag;
+        return pauseFlag_;
     }
 
     private IEnumerator Coroutine()
@@ -72,8 +80,7 @@ public class GameScene : MonoBehaviour
         //１秒待機
         // WaitForSecondsRealtimeにすることでTimeScaleの影響を受けない
         yield return new WaitForSecondsRealtime(1.0f);
-        seconds++;        // 1秒経過したらcount_が+1される
+        seconds_++;        // 1秒経過したらcount_が+1される
         secondsAddFlag_ = true;
     }
-
 }
