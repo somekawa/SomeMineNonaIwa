1be4b283645aab7571b4acb68362f21158dfc72b
saki <aaa>
2021-06-30 13:14:23 +0900

    消した部分が復活してたので消しました。 あと、時間の変数はクリアシーンで使うのでpublic staticにしています
    ・KeyTextを配列にしました
    Sizeを2
    Element0にはPauseUI内にある【現在取得している鍵の数】
    Element1にはBatteryCanvas内にある【KeyNum】
    
    変数名を整えてくれたのは助かりますが、
    他の更新内容をしっかり見てから直してください。

--- Scripts/Scene/GameScene.cs
+++ Scripts/Scene/GameScene.cs
@@ -8,63 +8,70 @@ public class GameScene : MonoBehaviour
 {
     public tBatteryScript batteryScript;
     public Text batteryText;
-    public Text keyText;                         // 現在取得している鍵の数
-    public PlayerCollision playerColScript;      // itemhitareaのスクリプト
-    public Text timerText;                       // タイマー表示用テキスト
+    public Text[] keyText;
+    public PlayerCollision playerColScript;
 
     private bool pauseFlag_;
-    private int minute_;                          // 何分か
-    private int seconds_;                        // 何秒か
-    private bool secondsAddFlag_;                // 秒数が足されたか
-    private bool resetFlag_;                     // 経過時間をリセットして良いか
+
+    // クリアシーンで使うため分と秒はpublicに
+    public static int minute;           // 何分か
+    public static int seconds;         // 何秒か
+    public Text timerText;        // タイマー表示用テキスト
+    private bool secondsAddFlag_; // 秒数が足されたか
 
     void Start()
     {
-        seconds_ = 0;
+        minute = 0;
+        seconds = 0;
         secondsAddFlag_ = false;
-        resetFlag_ = false;
         StartCoroutine("Coroutine");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //SceneManager.LoadScene("ClearScene");
+        // ポーズ（メニュー）を開く処理
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            if(pauseFlag_ != true)
-            {
-                pauseFlag_ = true;
-            }
-            else
-            {
-                pauseFlag_ = false;
-            }
-        }
-        batteryText.text = "電池残量" + batteryScript.ReturnBatteryRest()+"%";
-
-        // チュートリアルからメインに移った時だけはいる
-        if (resetFlag_==false&& SceneManager.GetActiveScene().name == "MainScene")
-        {
-            resetFlag_ = true;
-            seconds_ = 0;
-            StartCoroutine("Coroutine");
+            pauseFlag_ = !pauseFlag_;
         }
 
-        // 毎フレーム呼ばれないようにフラグで管理
+        ActiveText();
+
+        // 時間計測毎フレーム呼ばれないようにフラグで管理
         if (secondsAddFlag_ == true)
         {
-            // count_60以上＝1分
-            if (seconds_ >= 60f)
+            if (seconds >= 60f)
             {
-                minute_++;
-                seconds_ = seconds_ - 60;
+                // count_60以上＝1分
+                minute++;
+                seconds = seconds - 60;
             }
             StartCoroutine("Coroutine");
             secondsAddFlag_ = false;
         }
-        timerText.text = minute_.ToString("00") + ":" + (seconds_).ToString("00");
-        Debug.Log("Coroutine"+ seconds_);
+        timerText.text = minute.ToString("00") + ":" + (seconds).ToString("00");
+        // Debug.Log("Coroutine"+ seconds);
+
+    }
+
+    private void ActiveText()
+    {
+        if (SceneManager.GetActiveScene().name == "TutorialScene")
+        {
+            // チュートリアル中は電池の消費なしのため
+            batteryText.text = "電池残量100%";
+            // チュートリアル中は基礎行動で1つ、実践で1つ
+            keyText[0].text = "(" + playerColScript.GetkeyItemCnt() + "/ 1)";
+            keyText[1].text = "×\n" + playerColScript.GetkeyItemCnt();
+        }
+        else
+        {
+            // バッテリーの残り
+            batteryText.text = "電池残量" + batteryScript.ReturnBatteryRest() + "%";
+            // 鍵の所持数表示    // [0]ポーズ画面で　[1]ゲーム中
+            keyText[0].text = "(" + playerColScript.GetkeyItemCnt() + "/ 8)";
+            keyText[1].text = "×\n" + playerColScript.GetkeyItemCnt();
+        }
     }
 
     public bool GetPauseFlag()
@@ -75,12 +82,13 @@ public class GameScene : MonoBehaviour
     private IEnumerator Coroutine()
     {
         //処理１
-        Debug.Log("Start");
+       // Debug.Log("Start");
 
         //１秒待機
         // WaitForSecondsRealtimeにすることでTimeScaleの影響を受けない
         yield return new WaitForSecondsRealtime(1.0f);
-        seconds_++;        // 1秒経過したらcount_が+1される
+        seconds++;        // 1秒経過したらcount_が+1される
         secondsAddFlag_ = true;
     }
+
 }
