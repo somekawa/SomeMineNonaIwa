ec13f60f68ba980fde385090537e8d2bee98660c
saki <aaa>
2021-07-01 10:32:57 +0900

    取得したアイテムのUIのアクティブ状態を切り替えるscript 各Mngに追加してください
    BarrierにBarrierScript
    ItemTrhowにItemTrhowScript
    BarrierUIにBatteryCanvas-Items-BarrierBox内にあるBarrierBox(1)
    BottleUIにBatteryCanvas-Items-BottleBox内にあるBottleBox(1)

---
+++ Scripts/Item/ItemUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemUI : MonoBehaviour
+{
+    public Barrier barrier;
+    public ItemTrhow itemTrhow;
+
+    public GameObject barrierUI;
+    public GameObject bottoleUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (barrier.GetBarrierItemFlg() == true)
+        {
+            barrierUI.SetActive(true);
+        }
+        else
+        {
+            barrierUI.SetActive(false);
+        }
+        if (itemTrhow.GetTrhowItemFlg() == true)
+        {
+            bottoleUI.SetActive(true);
+        }
+        else
+        {
+            bottoleUI.SetActive(false);
+        }
+
+    }
+}
