d5c592b4becc2606176789302a969315bcb09c89
DAISUKEIWASAKI0615 <1816004@s.asojuku.ac.jp>
2021-07-02 12:35:21 +0900

    ドアを開けたらクリアシーンに遷移するよう調整

--- Scripts/StageObject/DoorAnimation.cs
+++ Scripts/StageObject/DoorAnimation.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DoorAnimation : MonoBehaviour
 {
@@ -10,8 +11,8 @@ public class DoorAnimation : MonoBehaviour
 
     private float minAngle_;            // 回転する前の角度
     private float maxAngle_;            // 回転した後の角度
-    private float tmpAngle_;               // 回転角度格納用変数
-    private bool flag;
+    private float tmpAngle_;            // 回転角度格納用変数
+    private bool transFlag;             // 遷移前の管理フラグ
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +21,7 @@ public class DoorAnimation : MonoBehaviour
         maxAngle_ = -5f;
         openFlag = false;
         closeFlag = false;
-        flag = false;
+        transFlag = false;
         tmpAngle_ = 0;
     }
 
@@ -32,21 +33,21 @@ public class DoorAnimation : MonoBehaviour
         //    openFlag = true;
         //}
         // 鎖と南京錠の数を調べて、0以下の時に扉がアニメーションするようにフラグを立てる
-        if (door.transform.childCount <= 0&&flag==false)
+        if (door.transform.childCount <= 0&& transFlag == false)
         {
-            flag = true;
+            transFlag = true;
             openFlag = true;
         }
 
         if (openFlag == true)
         {
             float step = -120f * Time.deltaTime;
-            tmpAngle_ += step;
+            tmpAngle_ += step;          // いくら回転したのか格納する
             //指定した方向にゆっくり回転する場合
             door.transform.rotation = Quaternion.RotateTowards(door.transform.rotation, Quaternion.Euler(0, maxAngle_, 0), step);
             //door.transform.Rotate(0f, step, 0f);
 
-            if (tmpAngle_ <= -90f)
+            if (tmpAngle_ <= -90f)      // 回転した角度を参照
             {
                 openFlag = false;
             }
@@ -67,6 +68,11 @@ public class DoorAnimation : MonoBehaviour
         else
         {
             return;
+        }
+
+        if (transFlag == true && openFlag == false)
+        {
+            SceneManager.LoadScene("ClearScene");
         }
     }
 }
