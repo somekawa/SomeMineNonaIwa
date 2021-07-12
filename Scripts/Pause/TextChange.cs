ab29c0ab414302d89dd1a86c9ce05e034087bf92
somekawa <1816044@s.asojuku.ac.jp>
2021-07-12 14:41:53 +0900

    コメント追加しました

--- Scripts/Pause/TextChange.cs
+++ Scripts/Pause/TextChange.cs
@@ -5,21 +5,19 @@ using UnityEngine.UI;
 
 public class TextChange : MonoBehaviour
 {
-    private List<Text> textUIList = new List<Text>();
-    private List<string> textOrigin = new List<string>();   // 元テキストの保存
     public GameObject TextMng;
+    private List<Text> textUIList_ = new List<Text>();       // 書き換え用テキスト
+    private List<string> textOrigin_ = new List<string>();   // 元テキストの保存
+    private string[] strArray_ = { "蟯ｩ蟠", "迹ｳ蟶", "驥惹ｸｭ", "譟灘ｷ" };   // 代入文字
 
-    private string[] strArray_ = { "蟯ｩ蟠", "迹ｳ蟶", "驥惹ｸｭ", "譟灘ｷ" };
-    //private float changeTimeMax_  = 0.3f;
-    private float changeTimeMax_ = 40.0f;
-    private float textChangeTime_ = 0.0f;
-    private bool textChangeFlg_   = false;
+    private float changeTimeMax_  = 45.0f;  // 文字の変更時間の最大値
+    private float textChangeTime_ = 0.0f;   // 現在の変更時間
+    private bool textChangeFlg_   = false;  // 変更中はフラグをtrueにする
 
-    //private float coolTime_    = 5.0f;
-    private float coolTime_ = 300.0f;
-    private float coolTimeMax_ = 5.0f;
+    private float coolTime_    = 300.0f;    // 現在の文字を変更しない時間
+    private float coolTimeMax_ = 300.0f;    // 文字を変更しない時間の最大値
 
-    private int rangeNum_ = -1;
+    private int rangeNum_ = -1;             // 書き換えるテキストの値を保存する
 
     // Start is called before the first frame update
     void Start()
@@ -27,8 +25,8 @@ public class TextChange : MonoBehaviour
         for(int i = 0; i < TextMng.transform.childCount; i++)
         {
             // TextMngの子を上から順番に取り出して、listに格納していく
-            textUIList.Add(TextMng.transform.GetChild(i).GetComponentInChildren<Text>());
-            textOrigin.Add(TextMng.transform.GetChild(i).GetComponentInChildren<Text>().text);
+            textUIList_.Add(TextMng.transform.GetChild(i).GetComponentInChildren<Text>());
+            textOrigin_.Add(TextMng.transform.GetChild(i).GetComponentInChildren<Text>().text);
         }
     }
 
@@ -39,13 +37,12 @@ public class TextChange : MonoBehaviour
         if (textChangeFlg_)
         {
             // 表示時間の加算
-            //textChangeTime_ += Time.deltaTime;
             textChangeTime_++;
 
             // 一定時間の経過
             if (textChangeTime_ >= changeTimeMax_)
             {
-                Change(textOrigin[rangeNum_]);
+                Change(textOrigin_[rangeNum_]);
                 textChangeTime_ = 0.0f;
                 rangeNum_ = -1;
             }
@@ -55,7 +52,6 @@ public class TextChange : MonoBehaviour
             if(coolTime_ <= coolTimeMax_)
             {
                 // クールタイム以下なら下の処理に進まない
-                //coolTime_ += Time.deltaTime;
                 coolTime_++;
                 return;
             }
@@ -63,7 +59,7 @@ public class TextChange : MonoBehaviour
             // 範囲0～9
             if (Random.Range(0, 10) == 0)    // 0のとき
             {
-                rangeNum_ = Random.Range(0, textUIList.Count); 　　
+                rangeNum_ = Random.Range(0, textUIList_.Count); 　　
                 Change(strArray_[Random.Range(0, 4)]);    
                 coolTime_ = 0.0f;
             }
@@ -79,8 +75,8 @@ public class TextChange : MonoBehaviour
         }
 
         // 文字反転,出力文字変更
-        textUIList[rangeNum_].transform.Rotate(new Vector3(180.0f, 0.0f, 0.0f));
-        textUIList[rangeNum_].text = tmpStr;
+        textUIList_[rangeNum_].transform.Rotate(new Vector3(180.0f, 0.0f, 0.0f));
+        textUIList_[rangeNum_].text = tmpStr;
         textChangeFlg_ = !textChangeFlg_;
     }
 }
