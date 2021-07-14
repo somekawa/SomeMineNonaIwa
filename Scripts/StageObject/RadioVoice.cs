7da348dc0ea1b3b9878ce76047864004a6aa2a4d
somekawa <1816044@s.asojuku.ac.jp>
2021-07-14 13:29:54 +0900

    ラジオとその設置台をPrefabにしました。
    RadioVintageには、RadioVoiceというScriptをアタッチしてください。
    RadioVoiceというScriptの変数は3_ahhahhaというSEを入れてください。
    Table_Bedという設置台の方は、もともと入っているAbandoned_Asylumから使用しています。
    
    アイテムを拾うのとどうように近づいてEキーを押すと20秒再生されます。
    普通に怖かった。。。

---
+++ Scripts/StageObject/RadioVoice.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioVoice : MonoBehaviour
+{
+    public AudioClip VoiceSound;
+    private bool soundFlg_ = false;
+    private float voiceTimeMax = 20.0f;
+    private float nowVoiceTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!soundFlg_)
+        {
+            return;
+        }
+
+        if(nowVoiceTime >= voiceTimeMax)
+        {
+            GetComponent<AudioSource>().Stop();
+            soundFlg_ = false;
+            nowVoiceTime = 0.0f;
+        }
+        else
+        {
+            nowVoiceTime += Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "ItemHitArea" && Input.GetKeyUp(KeyCode.E))
+        {
+            if (VoiceSound != null)
+            {
+                GetComponent<AudioSource>().clip = VoiceSound;
+                GetComponent<AudioSource>().Play();
+                soundFlg_ = true;
+            }
+        }
+    }
+}
