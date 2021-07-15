cfd4c28e85e90383e67c09280b15b21944a329ab
somekawa <1816044@s.asojuku.ac.jp>
2021-07-15 14:41:26 +0900

    BreakObjに関するもので不足しているものがあったため追加
    BrakObjHitのScriptは、BirdBathに設定してください。
    変数は
    DestructionObj→BirdBath-Destroyed
    DestructionEffect→Porcelain Debris
    を設定してください

---
+++ Scripts/StageObject/BreakObjHit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BreakObjHit : MonoBehaviour
+{
+
+    public GameObject DestructionObj;
+    public GameObject DestructionEffect;
+
+    // アクティブにするだけ
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "RecognitionCylinder")
+        {
+            if (DestructionObj != null) DestructionObj.SetActive(true);  // 破壊後のPrefabをアクティブ
+            if (DestructionEffect != null) DestructionEffect.SetActive(true);  // 破壊時のエフェクト
+            SoundScript.GetInstance().PlaySound(5);
+            Destroy(this.gameObject);
+        }
+    }
+}
