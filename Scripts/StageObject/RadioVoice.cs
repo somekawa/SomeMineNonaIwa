06de8c3df217973f79e84ea7be7ee912925d66bd
somekawa <1816044@s.asojuku.ac.jp>
2021-07-29 12:46:56 +0900

    ラジオの一括管理を行うためにPrefabとScriptを追加+修正しました
    RadioVoiceのScriptの引数には、RadioMngの子にあるVoiceAudioを設定してください。
    voiceAudioのScriptの引数には、3_ahhahhaのSEを設定してください。

--- Scripts/StageObject/RadioVoice.cs
+++ Scripts/StageObject/RadioVoice.cs
@@ -2,42 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class RadioVoice : MonoBehaviour
 {
-    public AudioClip VoiceSound;
+    public GameObject voiceAudio;
+    private RadioVoiceAudio voiceAudio_;
     private AudioSource source_;
 
     private bool soundFlg_ = false;
-    private float voiceTimeMax = 20.0f;
-    private float nowVoiceTime = 0.0f;
-
-    private float radioOnOffTime_ = 0.0f;
 
     // Sliderのワープ関連変数
-    //private GameObject[] slenderMan_;
     private SlenderManCtl[] slenderManCtl_;
-    //private float minDistance_;
-    //private float nowDistance_;
-    //private int minCnt_;
 
     void Start()
     {
-        // AudioSourceの情報を最初に取得しておく
-        source_ = GetComponent<AudioSource>();
-        //slenderMan_ = new GameObject[4];
+        voiceAudio_ = voiceAudio.GetComponent<RadioVoiceAudio>();
         slenderManCtl_ = new SlenderManCtl[4];
     }
 
     void Update()
     {
-        if (radioOnOffTime_ > 0.0f)
-        {
-            // ラジオ操作を受け付けない
-            radioOnOffTime_ -= Time.deltaTime;
-        }
-
         for (int i = 0; i < SlenderSpawner.GetInstance().spawnSlender.Length; i++)
         {
             if (slenderManCtl_[i] == null && SlenderSpawner.GetInstance().spawnSlender[i] != null)
@@ -54,74 +37,15 @@ public class RadioVoice : MonoBehaviour
         else
         {
             SlenderSpawner.GetInstance().ClosestObject(this.gameObject, 2, true, false);
-            //for (int x = 0; x < SlenderSpawner.GetInstance().spawnSlender.Length; x++)
-            //{
-            //    if (slenderMan_[x] != null)
-            //    {
-            //        nowDistance_ = Vector3.Distance(gameObject.transform.position, slenderMan_[x].transform.position);
-            //        if (minDistance_ >= nowDistance_)
-            //        {
-            //            minDistance_ = nowDistance_;
-            //            minCnt_ = x;
-            //        }
-            //    }
-            //}
-            //if (slenderManCtl_ != null)
-            //{
-            //    slenderManCtl_[minCnt_].soundPoint.x = this.gameObject.transform.position.x;
-            //    slenderManCtl_[minCnt_].soundPoint.z = this.gameObject.transform.position.z;
-            //    slenderManCtl_[minCnt_].navMeshAgent_.stoppingDistance = 2;
-            //    slenderManCtl_[minCnt_].listenFlag = true;
-            //    slenderManCtl_[minCnt_].ringingFlag = true;
-            //}
-        }
-
-        if (nowVoiceTime >= voiceTimeMax)
-        {
-            CommonSoundStop();
-            //slenderManCtl_[minCnt_].listenFlag = false;
-        }
-        else
-        {
-            nowVoiceTime += Time.deltaTime;
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (radioOnOffTime_ > 0.0f)
-        {
-            return;
-        }
-
         if (other.gameObject.tag == "ItemHitArea" && Input.GetKeyUp(KeyCode.E))
         {
-            if (!soundFlg_)   // SEのON
-            {
-                if (VoiceSound != null)
-                {
-                    source_.clip = VoiceSound;
-                    source_.Play();
-                    soundFlg_ = true;
-                    //Debug.Log("SEのON");
-                    radioOnOffTime_ = 1.0f;
-                    return;
-                }
-            }
-            else              // SEのOFF
-            {
-                CommonSoundStop();
-                //Debug.Log("SEのOFF");
-                radioOnOffTime_ = 1.0f;
-            }
+            // RadioVoiceAudioのSounds関数を呼ぶ
+            voiceAudio_.Sounds(int.Parse(this.gameObject.name), this.gameObject.transform.position);
         }
-    }
-
-    // 音再生の共通ストップ処理
-    void CommonSoundStop()
-    {
-        source_.Stop();
-        soundFlg_ = false;
-        nowVoiceTime = 0.0f;
     }
 }
---
+++ Scripts/StageObject/RadioVoiceAudio.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ラジオを1つ鳴らしている間は、他のラジオを再生できないようにしています
+
+public class RadioVoiceAudio : MonoBehaviour
+{
+    public AudioClip VoiceSound;
+
+    private AudioSource source_;
+    private float radioOnOffTime_ = 0.0f;
+    private bool soundFlg_ = false;
+    private float voiceTimeMax = 20.0f;
+    private float nowVoiceTime = 0.0f;
+
+    private int radioNum_ = -1;
+
+    void Start()
+    {
+        // AudioSourceの情報を最初に取得しておく
+        source_ = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (radioOnOffTime_ > 0.0f)
+        {
+            // ラジオ操作を受け付けない
+            radioOnOffTime_ -= Time.deltaTime;
+        }
+
+        if (!soundFlg_)
+        {
+            return;
+        }
+
+        if (nowVoiceTime >= voiceTimeMax)
+        {
+            CommonSoundStop();
+        }
+        else
+        {
+            nowVoiceTime += Time.deltaTime;
+        }
+
+    }
+
+    public void Sounds(int radioNum,Vector3 pos)
+    {
+        if (radioOnOffTime_ > 0.0f)
+        {
+            return;
+        }
+
+        if (!soundFlg_)   // SEのON
+        {
+            if (VoiceSound != null)
+            {
+                source_.clip = VoiceSound;
+                source_.Play();
+                soundFlg_ = true;
+                Debug.Log("SEのON");
+                radioOnOffTime_ = 1.0f;
+
+                // ラジオ番号の受け取り
+                radioNum_ = radioNum;
+                // VoiceAudioのオブジェクトを再生中のラジオの場所に移動させる
+                this.gameObject.transform.position = new Vector3(pos.x, pos.y, pos.z);
+                return;
+            }
+        }
+        else              // SEのOFF
+        {
+            // 再生中の同じラジオじゃないと停止できないようにする
+            if(radioNum == radioNum_)
+            {
+                CommonSoundStop();
+                Debug.Log("SEのOFF");
+                radioOnOffTime_ = 1.0f;
+            }
+            else
+            {
+                Debug.Log("別のラジオの為、OFFにできない");
+            }
+        }
+    }
+
+    // 音再生の共通ストップ処理
+    void CommonSoundStop()
+    {
+        source_.Stop();
+        soundFlg_ = false;
+        nowVoiceTime = 0.0f;
+    }
+
+    public bool GetSoundFlg()
+    {
+        return soundFlg_;
+    }
+}
