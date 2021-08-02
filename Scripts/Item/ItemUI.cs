e13f5405b98e71d686d67df91f10f02ce7d2fd49
saki <aaa>
2021-08-02 12:23:29 +0900

    Bottle処理を削除しました

--- Scripts/Item/ItemUI.cs
+++ Scripts/Item/ItemUI.cs
@@ -4,26 +4,18 @@ using UnityEngine;
 
 public class ItemUI : MonoBehaviour
 {
-    // Component取得用変数
     public Barrier barrier;
-    public ItemTrhow itemTrhow;
 
     public GameObject barrierUI;
-    public GameObject bottoleUI;
 
-    // Start is called before the first frame update
     void Start()
     {
         barrierUI.SetActive(false);
-        bottoleUI.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // テスト中
-        //barrierUI.SetActive(barrier.GetBarrierItemFlg() == true ? true : false);
-        //bottoleUI.SetActive(itemTrhow.GetTrhowItemFlg() == true ? true : false);
 
         if (barrier.GetBarrierItemFlg() == true)
         {
@@ -32,14 +24,6 @@ public class ItemUI : MonoBehaviour
         else
         {
             barrierUI.SetActive(false);
-        }
-        if (itemTrhow.GetTrhowItemFlg() == true)
-        {
-            bottoleUI.SetActive(true);
-        }
-        else
-        {
-            bottoleUI.SetActive(false);
         }
     }
 }
