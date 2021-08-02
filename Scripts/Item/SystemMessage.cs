dbb01beed5c35b9956452bc030e8a924b3a43d49
saki <aaa>
2021-08-02 10:52:33 +0900

    ・GameSceneScript（各シーンのMngにある） チュートリアル中はcollect関連処理に入らないようにしました。 ゲームが始まった瞬間にpauseUIが開けない処理はそのままにしています。 鍵の所持数表示の配列がわかりにくかったので2つに分けました。 pauseKeyTextはPauseUIの「現在取得している鍵の数」 mainKeyTextはBatteryCanvas-items-KeyImage-「KeyNum」
    ・SystemMessageScript（UIMngにある）
    ラジオとボックスのテキスト表示を追加
    HideCtlにHideContorolScript
    CollisionにPlayerCollision
    MainCameraにPlayer-「MainCamera」
    TextBackにBatteryCanvas-「MessageBack」
    TextMessageにMessageBack-「SystemText」
    LeanMessageにMessageBack-「LeanText」
    
    ・tBatteryScript（BatteryCanvas-Batteriesにある）
    消費電池表示の配列がわかりにくかったのでGetChildで取得に変更しました
    
    ・その他チュートリアル関連
    電池関連がないため、tBatteryScriptは非アクティブに
    Sanがそもそもなくすぐ文字化けになるためTextChangeScriptを非アクティブに
    MapCanvasのFullMapとMiniMapのRawImageを削除。半透明のImageを表示

--- Scripts/Item/SystemMessage.cs
+++ Scripts/Item/SystemMessage.cs
@@ -7,53 +7,78 @@ public class SystemMessage : MonoBehaviour
 {
     public enum action
     {
-
         NON,
-        BATTERY,
-        BARRIER_GET,
-        // BARRIER_USE,
-        BOTTLE_GET,
-        //BOTTLE_TRHOW,
-        KEY,
+        BATTERY,    // 電池を取得した
+        BARRIER_GET,// バリアを取得した
+        KEY,        // 鍵を取得した
+        RADIO_USE,  // ラジオを使えます
+        BOX_IN,     // 隠れる
+        BOX_OUT,    // 隠れてる状態から出る
+        LEAN,       // 傾けれる
         MAX
     }
     private action action_;
-        
 
-    public PlayerCollision collision;// どのアイテムを取得したかの確認
+    public HideControl hideCtl;     // 隠れているかのチェック
+    public PlayerCollision collision;   // どのアイテムを取得したかの確認
+    public GameObject mainCamera;  // player側のカメラ
 
-    public Image textBack;
-    public Text textMessage;
-    private float alpha_;// テキストのアルファ値を変更
-    private string[] message_;
-    private bool activeFlag_;
+    public Image textBack;              // 文字の背景
+    public Text textMessage;            // 表示する文字
+    public GameObject leanMessage;      // レーン用の文字
+
+    private HideBox hideBox_;        // ボックスとプレイヤーの接触状態
+    private RadioVoice radioVoice_;   // ラジオを使える時
+
+    private float alpha_ = 1.0f;    // テキストのアルファ値を変更
+    private bool textActiveFlag_ = false;
+    private string[] message_;      // 行動表示の文字を保存
 
     void Start()
     {
-        alpha_ = 255.0f;
-        activeFlag_ = false;
+        // 隠れる箱を探す
+        GameObject hideObj = GameObject.Find("MannequinBox").gameObject;
+        hideBox_ = hideObj.GetComponent<HideBox>();
+
+        // ラジオを見つける
+        GameObject radioObj = GameObject.Find("RadioVintage").gameObject;
+        radioVoice_ = radioObj.GetComponent<RadioVoice>();
+
         action_ = action.NON;
-        message_ = new string[(int)action.MAX]{"","ライトの充電をします",  
-            "防御アイテムを拾いました" ,"誘導アイテムを拾いました","鍵を入手しました"};
+        message_ = new string[(int)action.MAX]{"","ライトを充電しました",
+            "防御アイテムを拾いました" , "鍵を拾いました",
+            "Eキー：ラジオを使う", "Eキー：隠れる","Eキー：出る","Tキー：通路を覗き込む"};
         textMessage.text = message_[(int)action.NON];
         textBack.enabled = false;
     }
 
     void Update()
     {
-        if (activeFlag_ == true)
+        if (textActiveFlag_ == false)
         {
-            // actionした時だけはいる
-            CheckMessage(action_);
+            // メッセージが表示されていないとき
+            ItemAction();
         }
         else
         {
-             ItemAction();
+            if (action.RADIO_USE <= action_)
+            {
+                // ラジオ以降（〇〇できる系）
+                // 行動を起こすまで表示
+                ActionMessage(action_);
+            }
+            else
+            {
+                // 鍵までの（○○しました系）
+                // 表示したら徐々に消していく
+                ItemGetMessage(action_);
+            }
         }
     }
 
     private void ItemAction()
     {
+        // どのアイテムを拾ったかplayerCollisionから確認する
         for (int i = 0; i < (int)PlayerCollision.item.MAX; i++)
         {
             if (collision.GetItemNum() != PlayerCollision.item.NON)
@@ -62,39 +87,80 @@ public class SystemMessage : MonoBehaviour
                 if (collision.GetItemNum() == (PlayerCollision.item)i)
                 {
                     action_ = (action)collision.GetItemNum();
-                    activeFlag_ = true;
                 }
             }
         }
+
+        // ラジオを使える範囲にいる時
+        if (radioVoice_.GetRadioAround() == true)
+        {
+            action_ = action.RADIO_USE;
+        }
+
+        // ボックスを使える範囲にいる時
+        if (hideBox_.InFlagCheck() == true)
+        {
+            action_ = action.BOX_IN;
+        }
+
+        // NON以外＝何らかの行動をしているからテキストを表示
+        if (action_!=action.NON)
+        {
+            textActiveFlag_ = true;
+        }
     }
 
-    private void CheckMessage(action text)
+    private void ActionMessage(action text)
     {
-        if (action_ != action.NON)
+        if (mainCamera.activeSelf == true       // プレイヤーのカメラがアクティブの時
+        && hideBox_.InFlagCheck() == false        // ボックスの範囲外
+        && radioVoice_.GetRadioAround() == false) // ラジオの範囲外
         {
-            textMessage.text = message_[(int)text];
-            if (alpha_ <= 0)
-            {
-                textMessage.text = message_[(int)action.NON];// 何も表示しない状態
-                action_ = action.NON;// どのアクションでもない
-                collision.SetItemNum(PlayerCollision.item.NON);// ItemNumをリセット
-                activeFlag_ = false;// テキストのアクティブ状態を変更
-                alpha_ = 255.0f;
-                textBack.enabled = false;
-            }
-            else
-            {
-                // 表示中　透明度を下げていく
-                alpha_ -= 2.0f;
-                textMessage.color = new Color(0, 0, 0, alpha_);
-                textBack.enabled = true;
-            }
+            // プレイヤーのカメラがアクティブの時かつボックスの範囲外の時
+            textMessage.text = message_[(int)action.NON]; // 何も表示しない状態
+            textActiveFlag_ = false;        // テキストのアクティブ状態を変更 
+            textBack.enabled = false; // 背景を非表示
+            action_ = action.NON;     // どのアクションでもない
         }
         else
         {
-            textMessage.text = message_[(int)action.NON];// 何も表示しない状態
+            // 箱から出る時
+            if (mainCamera.activeSelf == false && hideBox_.InFlagCheck() == false)
+            {
+                text = action.BOX_OUT; 
+            }
 
+            // 受け取った行動のものを表示する
+            textBack.enabled = true;
+            textMessage.text = message_[(int)text];
+            alpha_ = 1.0f;
+            textMessage.color = new Color(0.0f, 0.0f, 0.0f, alpha_);
+            textBack.color = new Color(255.0f, 255.0f, 255.0f, alpha_);
         }
-      //  Debug.Log(alpha_);
+    }
+
+    private void ItemGetMessage(action text)
+    {
+        // アイテムを取得した時のメッセージ表示
+        textMessage.text = message_[(int)text];
+        textBack.enabled = true;
+        if (0.0f <= alpha_)
+        {           
+            // 表示中　
+            leanMessage.SetActive(false);    // アイテム文字表示中に出ないように
+            alpha_ -= 0.005f;                // 透明度を下げていく
+            textMessage.color = new Color(0.0f, 0.0f, 0.0f, alpha_);
+            textBack.color = new Color(255.0f, 255.0f, 255.0f, alpha_);
+        }
+        else
+        {
+            // アルファ値が0になったら非表示に
+            textMessage.text = message_[(int)action.NON];       // 何も表示しない状態
+            collision.SetItemNum(PlayerCollision.item.NON);     // ItemNumをリセット
+            textBack.enabled = false;   // 背景を非表示
+            action_ = action.NON;       // どのアクションでもない
+            alpha_ = 1.0f;              // アルファ値をリセット
+            textActiveFlag_ = false;          // テキストのアクティブ状態を変更
+        }
     }
 }
