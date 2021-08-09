93169805da204a74294144fb9cd974d86fd54578
saki <aaa>
2021-08-09 15:07:43 +0900

    更新ミス。特に変更はないのでこれを入れておいてください

--- Scripts/Scene/Clear/CharacterLoop.cs
+++ Scripts/Scene/Clear/CharacterLoop.cs
@@ -4,31 +4,20 @@ using UnityEngine;
 
 public class CharacterLoop : MonoBehaviour
 {
-    //public ResultScript result;
     public GameObject Player;     // プレイヤーを取得
     private float speed_ = 4.5f;  // プレイヤーの動くスピード
     private float moveZ_ = 10.0f;  // プレイヤーの初期のZ座標
 
     void Start()
     {
-
     }
 
     void Update()
     {
-        if (Player.GetComponent<CharacterCtl>().enabled == true)
-        {
-            Player.transform.position = new Vector3(15.0f, 1.0f, 4.5f);
-            Destroy(this);
-        }
-        else
-        {
-            // 移動させる
-            moveZ_ += speed_ * Time.deltaTime;
-            Player.transform.position = new Vector3(15.0f, 1.0f, moveZ_);
+        // 移動させる
+        moveZ_ += speed_ * Time.deltaTime;
+        Player.transform.position = new Vector3(15.0f, 1.0f, moveZ_);
 
-        }
-        Debug.Log(Player.GetComponent<CharacterCtl>().enabled +"ループポイントからみたプレイヤーZ座標" + Player.transform.position.z);
     }
 
     private void OnTriggerEnter(Collider other)
